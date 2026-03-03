2bb0f1cf116adbb3d4f52d22ba5d9da1bca5b454
Barnamwork <barnamwork@gmail.com>
2026-03-03 12:47:04 +0530

    package com.bridgelabz;

--- src/main/java/com/bridgelabz/WatchServiceDemo.java
+++ src/main/java/com/bridgelabz/WatchServiceDemo.java
@@ -1,4 +1,33 @@
 package com.bridgelabz;
 
+import java.io.IOException;
+import java.nio.file.*;
+
 public class WatchServiceDemo {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Path path = Paths.get("TempDirectory");
+        Files.createDirectories(path);
+
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+
+        path.register(watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY);
+
+        System.out.println("Watching Directory...");
+
+        while (true) {
+            WatchKey key = watchService.take();
+
+            for (WatchEvent<?> event : key.pollEvents()) {
+                System.out.println("Event: " + event.kind()
+                        + " File: " + event.context());
+            }
+
+            key.reset();
+        }
+    }
 }
